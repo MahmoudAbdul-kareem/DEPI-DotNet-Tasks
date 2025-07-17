876e290b9d2b41d2b1fd9e74f70e56606e40cf43
MahmoudAbdul-kareem <mahmoudabdulkareemawad@gmail.com>
2025-07-17 10:55:37 +0300

    SQLServer Session-3 Tasks UPDATED

--- 1 - SQL Server/Session3-Assignment/StoreDB-Queries(Task-2).sql
+++ 1 - SQL Server/Session3-Assignment/StoreDB-Queries(Task-2).sql
@@ -1,3 +1,5 @@
+USE StoreDB;
+
 -- 1. List all products with list price greater than 1000
 SELECT * 
 FROM production.products
@@ -6,12 +8,12 @@ WHERE list_price > 1000;
 -- 2. Get customers from "CA" or "NY" states
 SELECT * 
 FROM sales.customers
-WHERE state IN ('CA', 'NY');
+WHERE state = 'CA' OR state = 'NY';
 
 -- 3. Retrieve all orders placed in 2023
 SELECT * 
 FROM sales.orders
-WHERE YEAR(order_date) = 2023;
+WHERE YEAR(order_date) = '2023';
 
 -- 4. Show customers whose emails end with @gmail.com
 SELECT * 
@@ -24,19 +26,19 @@ FROM sales.staffs
 WHERE active = 0;
 
 -- 6. List top 5 most expensive products
-SELECT TOP 5 * 
+SELECT TOP 5 *
 FROM production.products
 ORDER BY list_price DESC;
 
 -- 7. Show latest 10 orders sorted by date
-SELECT TOP 10 * 
+SELECT TOP 10 *
 FROM sales.orders
 ORDER BY order_date DESC;
 
 -- 8. Retrieve the first 3 customers alphabetically by last name
 SELECT TOP 3 * 
 FROM sales.customers
-ORDER BY last_name ASC;
+ORDER BY last_name;
 
 -- 9. Find customers who did not provide a phone number
 SELECT * 
@@ -49,39 +51,27 @@ FROM sales.staffs
 WHERE manager_id IS NOT NULL;
 
 -- 11. Count number of products in each category
-SELECT 
-    category_id,
-    COUNT(*) AS product_count
+SELECT category_id, COUNT(*) AS product_count
 FROM production.products
 GROUP BY category_id;
 
-
 -- 12. Count number of customers in each state
-SELECT 
-    state,
-    COUNT(customer_id) AS customer_count
+SELECT state, COUNT(customer_id) AS customer_count
 FROM sales.customers
 GROUP BY state;
 
 -- 13. Get average list price of products per brand
-SELECT 
-    brand_id,
-    AVG(list_price) AS avg_list_price
+SELECT brand_id, AVG(list_price) AS average_price
 FROM production.products
 GROUP BY brand_id;
 
-
 -- 14. Show number of orders per staff
-SELECT 
-    staff_id,
-    COUNT(*) AS order_count
+SELECT staff_id, COUNT(*) AS orders_count
 FROM sales.orders
 GROUP BY staff_id;
 
 -- 15. Find customers who made more than 2 orders
-SELECT 
-    customer_id,
-    COUNT(*) AS order_count
+SELECT customer_id, COUNT(*) AS order_count
 FROM sales.orders
 WHERE customer_id IS NOT NULL
 GROUP BY customer_id
@@ -98,9 +88,9 @@ FROM sales.customers
 WHERE city LIKE 'S%';
 
 -- 18. Orders with order_status either 2 or 4
-SELECT * 
+SELECT *
 FROM sales.orders
-WHERE order_status IN (2, 4);
+WHERE order_status = 2 OR order_status = 4;
 
 -- 19. Products from category_id IN (1, 2, 3)
 SELECT * 
@@ -110,5 +100,4 @@ WHERE category_id IN (1, 2, 3);
 -- 20. Staff working in store_id = 1 OR without phone number
 SELECT * 
 FROM sales.staffs
-WHERE store_id = 1 OR phone IS NULL OR phone = '';
-
+WHERE store_id = 1 OR phone IS NULL OR PHONE = '';
